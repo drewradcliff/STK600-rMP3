1eed7ced56d87d2bde860ce51319860214ef6b77
Drew <sirampr@gmail.com>
2018-12-17 10:13:03 -0600

    Create ports.h

---
+++ Ports.h
@@ -0,0 +1,15 @@
+/*
+* ports.h
+*
+* This file includes all the defines and prototypes used for the Ports
+*
+* Created: 4/30/2015 1:44:38 PM
+* Author: Drew Radcliff
+*/
+
+#ifndef PORTS_H_ //include macro guard
+#define PORTS_H_ //ensure independence
+
+//prototypes
+void init_ports(void);
+#endif /* PORTS_H_ */
